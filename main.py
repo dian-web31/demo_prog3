0c28e5723dc9e7ddb52d512f33451d62ce9fa1f6
dian-web31 <juan.henao36071@tools.ucaldas.edu.co>
2024-09-09 11:24:59 -0500

    agregamos division y multiplicacion

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def resta(x,y):
     return x-y
 
+def division(x,y):
+    return x/y
+
+def multiplicacion(x,y):
+    return x*y
+
+respuesta = multiplicacion(7,3)
+print(respuesta)
+respuesta = division(7,3)
+print(respuesta)
 respuesta = resta(7,3)
 print(respuesta)
