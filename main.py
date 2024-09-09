0cd3be0fd2ceb37f810472d055be2c8005d5f5a9
dian-web31 <juan.henao36071@tools.ucaldas.edu.co>
2024-09-09 10:53:14 -0500

    implementacion resta

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def suma(x,y):
     return x+y
 
-respuesta = suma(2,3)
+def resta(x,y):
+    return x-y
+
+respuesta = resta(7,3)
 print(respuesta)
+respuesta = suma(5,7)
+print("La respuesta es "+str(respuesta))
