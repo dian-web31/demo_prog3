9a443fcb5d9c18bac147f33824c9751feee87908
Sebastian-09 <juan.molina28801@ucaldas.edu.co>
2024-09-11 08:41:01 -0500

    Suma

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def division(x,y):
 def multiplicacion(x,y):
     return x*y
 
+def suma(x,y):
+    return x+y
+
 respuesta = multiplicacion(7,3)
 print(respuesta)
 respuesta = division(7,3)
 print(respuesta)
 respuesta = resta(7,3)
+print(respuesta)
+respuesta = suma(7,3)
 print(respuesta)
